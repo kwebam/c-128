5c8d860fe5f5268f95f55dd70d34932d6ef18528
kwebam <77285125+kwebam@users.noreply.github.com>
2021-06-03 13:38:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+song = "";
+
+function preload() {
+          song = loadSound("music.mp3");
+}
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function setup() {
+          canvas = createCanvas(600 , 500);
+          canvas.center()
+
+          video = createCapture(VIDEO);
+          video.hide()
+
+          poseNet = ml5.poseNet(video ,modelLoaded)
+          poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded() {
+          console.log("poseNet is Initialized");
+}
+
+function draw() {
+          image(video, 0, 0, 600, 500);
+}
+
+function play() {
+          song.play();
+          song.setVolume(1);
+          song.rate(1);
+}
+
+function gotPoses(results) {
+          if (results.length > 0) {
+                    console.log(results);
+                    rightWristX = results[0].pose.rightWrist.x;
+                    rightWristY = results[0].pose.rightWrist.y;
+                    console.log("Right Wrist X = " + rightWristX + " Right Wrist Y = " + rightWristY);
+                    leftWristX = results[0].pose.leftWrist.x;
+                    leftWristY = results[0].pose.leftWrist.y;
+                    console.log("Left Wrist X = " + leftWristX + " Left Wrist Y = " + leftWristY);
+
+          }
+}
